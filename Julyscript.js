a7280eb1d0f25fbdd99bce1ffd853e48228461c5
suwon <84270989+SuwonJ@users.noreply.github.com>
2023-12-08 15:09:02 +0900

    Update Julyscript.js
    Signed-off-by: suwon <84270989+SuwonJ@users.noreply.github.com>

--- Julyscript.js
+++ Julyscript.js
@@ -101,36 +101,21 @@ document.addEventListener("DOMContentLoaded", function() {
 /***************/
 
 
-var target_date = new Date().getTime() + (1000*3600*48); // set the countdown date
-var days, hours, minutes, seconds; // variables for time units
+const remainTime = document.querySelector("#remain-time");
 
-var countdown = document.getElementById("tiles"); // get tag element
-
-getCountdown();
-
-setInterval(function () { getCountdown(); }, 1000);
-
-function getCountdown(){
-
-	// find the amount of "seconds" between now and target
-	var current_date = new Date().getTime();
-	var seconds_left = (target_date - current_date) / 1000;
-
-	days = pad( parseInt(seconds_left / 86400) );
-	seconds_left = seconds_left % 86400;
-		 
-	hours = pad( parseInt(seconds_left / 3600) );
-	seconds_left = seconds_left % 3600;
-		  
-	minutes = pad( parseInt(seconds_left / 60) );
-	seconds = pad( parseInt( seconds_left % 60 ) );
-
-	// format countdown string + set tag value
-	countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>"; 
+function diffDay() {
+    const masTime = new Date("2022-12-25");
+    const todayTime = new Date();
+    
+    const diff = masTime - todayTime;
+    
+    const diffDay = Math.floor(diff / (1000*60*60*24));
+    const diffHour = Math.floor((diff / (1000*60*60)) % 24);
+    const diffMin = Math.floor((diff / (1000*60)) % 60);
+    const diffSec = Math.floor(diff / 1000 % 60);
+    
+    remainTime.innerText = `${diffDay}일 ${diffHour}시간 ${diffMin}분 ${diffSec}초`;
 }
 
-function pad(n) {
-	return (n < 10 ? '0' : '') + n;
-}
-
-
+diffDay();
+setInterval(diffDay, 1000);
